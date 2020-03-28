0570378188642a1fd3b89ae6a1fc4f8873e20d01
Anthony Zhou <anthony.zhou03@gmail.com>
2020-03-28 18:13:21 -0500

    Modified student view

---
+++ src/StudentView/RunButton.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RunButton() {
+  return (
+    <button id="run-button" onClick={handleClick}>
+        Run code
+    </button>
+  );
+}
+
+function handleClick(e) {
+
+}
+export default RunButton;
--- src/StudentView/StudentView.js
+++ src/StudentView/StudentView.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Problem from '../Problem.js';
 import AceEditor from "react-ace";
+import RunButton from './RunButton'
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/theme-monokai";
 
 function StudentView() {
   return (
@@ -10,10 +13,20 @@ function StudentView() {
         id="code-editor"
         mode="javascript"
         theme="monokai"
+        value={starterCode}
+        width="auto"
+        height="auto"
         name="code-editor"
         editorProps={{ $blockScrolling:true }}/>
+      <RunButton/>
     </div>
   );
 }
+
+const starterCode = `function main(nums) {
+  // Begin coding here
+  return 0;
+}
+`
 
 export default StudentView;
