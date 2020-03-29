864ac3de3a3524e701806efd782cb1d492bb2a21
Anthony Zhou <anthony.zhou03@gmail.com>
2020-03-28 23:39:28 -0500

    Updates

---
+++ precache-manifest.ca6a6d1b1c2455a62ef40514247145eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eeae9b7bfe0e84ea4bb88e31c55c5f5c",
+    "url": "/education-app/index.html"
+  },
+  {
+    "revision": "d69007e59026f8dda3ef",
+    "url": "/education-app/static/css/main.de0b9eb8.chunk.css"
+  },
+  {
+    "revision": "8382d6aaae9e92de0249",
+    "url": "/education-app/static/js/2.82c02658.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/education-app/static/js/2.82c02658.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d69007e59026f8dda3ef",
+    "url": "/education-app/static/js/main.a7405535.chunk.js"
+  },
+  {
+    "revision": "07061b7f899911f3225f",
+    "url": "/education-app/static/js/runtime-main.a97c3a62.js"
+  }
+]);
