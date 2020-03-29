ebd44850c0ca17677778f54bddf483e9066e7815
zach <zachtang0123@gmail.com>
2020-03-29 10:05:30 -0500

    called api to get student data to show

--- src/StudentView/IntroScreen.js
+++ src/StudentView/IntroScreen.js
@@ -4,11 +4,13 @@ class IntroScreen extends React.Component {
   render() {
     return (
       <div className = "intro-screen">
-      <div id = "wrapper">
-      <h1 style={{color: "white"}}>Welcome to the Education App!</h1>
-      <input type="text" placeholder="Type stuff"/>
-  <button>start coding!</button>
-  </div>
+        <div id = "wrapper">
+          <h1 style={{color: "white"}}>Welcome to the Education App!</h1>
+          <form action="/api/users" method="POST">  
+            <input type="text" placeholder="Type stuff"/>
+            <button>start coding!</button>
+          </form>
+        </div>
       </div>
     );
   }
